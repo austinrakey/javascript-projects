1d7e541eacd4cd1c1bb81f8d4bd8c9b612081374
austinrakey <rakeysound@gmail.com>
2024-01-08 20:26:15 -0600

    added punctuation, cleaned up

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -10,11 +10,11 @@
 
 // Print the results of the trip to the moon below
 
-console.log(typeof 'Determination')
-console.log(typeof 17500)
-console.log(typeof 225000000)
-console.log(typeof 384400)
-console.log(typeof 0.621)
+console.log(typeof 'Determination');
+console.log(typeof 17500);
+console.log(typeof 225000000);
+console.log(typeof 384400);
+console.log(typeof 0.621);
 
 let shuttleName = 'Determination';
 let shuttleSpeedMph = 17500;
@@ -25,10 +25,10 @@ const milesPerKm = 0.621;
 let milesToMars = distanceToMarsKm * milesPerKm;
 let hoursToMars = milesToMars / shuttleSpeedMph;
 let daysToMars = hoursToMars / 24;
-console.log(shuttleName + " will take " + daysToMars + " days to reach Mars ")
+console.log(shuttleName + " will take " + daysToMars + " days to reach Mars ");
 
 
 let milesToMoon = distanceToMoonKm * milesPerKm;
 let hoursToMoon = milesToMoon / shuttleSpeedMph;
 let daysToMoon = hoursToMoon / 24;
-console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon ")
+console.log(shuttleName + " will take " + daysToMoon + " days to reach the Moon ");

8368f0c1c77bdcfde8ba53fdf16d3e413f78d840
austinrakey <rakeysound@gmail.com>
2024-02-08 16:41:40 -0600

    unit test exercises

---
+++ unit-testing/exercises/tests/RPS.test.js
@@ -0,0 +1,21 @@
+const whoWon = require('../RPS.js');
+
+describe("whoWon", function(){
+
+test("returns TIE when both players choose the same option", function(){
+    let output = whoWon('rock', 'rock');
+    expect(output).toEqual('TIE!')
+    });
+
+test("returns 'Player 2 wins!' if P1 = rock & P2 = paper", function(){
+    let output = whoWon('rock','paper');
+    expect(output).toBe("Player 2 wins!");
+ });
+ 
+ test("returns 'Player 2 wins!' if P1 = paper & P2 = scissors", function(){
+    let output = whoWon('paper','scissors');
+    expect(output).toBe("Player 2 wins!");
+ });
+
+});
+

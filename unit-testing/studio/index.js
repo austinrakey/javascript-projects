e3303eed8a84979ff6e8067979a9cd076b283af9
austinrakey <rakeysound@gmail.com>
2024-02-08 20:54:27 -0600

    unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,34 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(num){
+   let statement = ""  
+    if (((num % 3) === 0) && ((num % 5) === 0) && ((num % 2) === 0)){
+    statement = 'LaunchCode Rocks!'
+    return statement
+  } else if (((num % 3) !== 0) && ((num % 5) === 0) && ((num % 2) === 0)) {
+    statement = 'Launch Rocks!'
+  } else if (((num % 3) === 0) && ((num % 5) === 0) && ((num % 2) !== 0)) {
+    statement = 'Code Rocks!'
+  } else if (((num % 3) === 0) && ((num % 5) !== 0) && ((num % 2) === 0)) {
+    statement = 'LaunchCode!'
+  } else if (((num % 3) !== 0) && ((num % 5) === 0) && ((num % 2) !== 0)) {
+    statement = 'Rocks!'
+  } else if (((num % 3) === 0) && ((num % 5) !== 0) && ((num % 2) !== 0)) {
+    statement = 'Code!'
+  } else if (((num % 3) !== 0) && ((num % 5) !== 0) && ((num % 2) === 0)) {
+    statement = 'Launch'
+  } else {
+    statement = "Rutabagas! That doesn't work."
 }
+return statement
+}
+}
+
+
 
 module.exports = launchcode;
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,38 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
-describe("Testing launchcode", function(){
+describe("testing launchcode object", function(){
+  it("key organization has value nonprofit", function() {
+    expect(launchcode.organization).toEqual("nonprofit")
+   });
 
+  it("key executiveDirector has value Jeff", function() {
+    expect(launchcode.executiveDirector).toEqual("Jeff")
+  });
+
+  it("key percentageCoolEmployees has value 100", function() {
+    expect(launchcode.percentageCoolEmployees).toEqual(100)
+  });
+
+  it("key programsOffered has values Web Development, Data Analysis, Liftoff", function() {
+    // expect(launchcode.programsOffered).toEqual(["Web Development", "Data Analysis", "Liftoff"])
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+    expect(launchcode.programsOffered.length).toBe(3)
+  });
+
+  it("launchOutput returns", function() {
+
+  expect(launchcode.launchOutput(2)).toEqual('Launch')
+  expect(launchcode.launchOutput(3)).toEqual('Code!')
+  expect(launchcode.launchOutput(5)).toEqual('Rocks!')
+  expect(launchcode.launchOutput(6)).toEqual('LaunchCode!')
+  expect(launchcode.launchOutput(15)).toEqual('Code Rocks!')
+  expect(launchcode.launchOutput(10)).toEqual('Launch Rocks!')
+  expect(launchcode.launchOutput(7)).toEqual("Rutabagas! That doesn't work.")
   // Write your unit tests here!
   
+});
+
 });

144b2b901fc96d5cd6a12b5bf190eea69b354847
austinrakey <rakeysound@gmail.com>
2024-03-04 19:59:57 -0600

    fetch studio

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,1 +1,24 @@
 //TODO: Add Your Code Below
+window.addEventListener("load", function() {
+this.fetch('https://handlers.education.launchcode.org/static/astronauts.json').then(function(response) {
+    return response.json();
+}).then(function(json) {
+console.log(json)
+const container = document.querySelector('#container');
+for (astronaut of json){
+container.innerHTML += `
+<div class="astronaut">
+<div class="bio">
+    <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+    <ul>
+    <li>Hours in space: ${astronaut.hoursInSpace}</li>
+    <li>Active: ${astronaut.active}</li>
+    <li>Skills: ${astronaut.skills.join(", ")}</li>
+    </ul>
+</div>
+<img class="avatar" src="${astronaut.picture}">
+</div>`
+};
+});
+});
+
